702800fa1172c8c100fa358661eaf83140c4e704
BIT <BIT@DESKTOP-VC61ESU>
2019-05-24 10:56:19 +0900

    xml 설정 기본 끝

--- src/main/java/com/cafe24/springcontainer/ContainerTest.java
+++ src/main/java/com/cafe24/springcontainer/ContainerTest.java
@@ -7,6 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
+import com.cafe24.springcontainer.user.User;
 import com.cafe24.springcontainer.user.User1;
 
 public class ContainerTest {
@@ -17,17 +18,36 @@ public class ContainerTest {
 	}
 	
 	public static void testApplicationContext() {
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("config/user/applicationContext.xml");
-		
-		User1 user = (User1)appContext.getBean("user1");
-		System.out.println(user.getName());
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("config/user/applicationContext2.xml");
 
-		user = appContext.getBean(User1.class);
-		System.out.println(user.getName());
-	
+		User1 user1 = appContext.getBean(User1.class);
+		System.out.println(user1.getName());
+
+		// 오류: 빈설정 id나 name 설정을 해야 한다.
+		//user1 = (User1)appContext.getBean("user1");
+		//System.out.println(user.getName());
+
+		// name으로 가져오기
+		User user = (User)appContext.getBean("user"); 
+		System.out.println(user);
 		
+		// id로 가져오기
+		user = (User)appContext.getBean("usr"); 
+		System.out.println(user);
 		
+		// 오류: 같은 타입의 빈이 2개이상 존재하면 
+		//      타입으로 빈을 가져올 수 없다. 
+		// user = appContext.getBean(User.class);
 		
+		User user2 = (User)appContext.getBean("usr2");
+		System.out.println(user2);
+		
+		User user3 = (User)appContext.getBean("usr3");
+		System.out.println(user3);
+
+		User user4 = (User)appContext.getBean("usr4");
+		System.out.println(user4);
+
 		((ConfigurableApplicationContext)appContext).close();
 	}
 
